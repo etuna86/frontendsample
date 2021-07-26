3e65d1814236293668bc60e21c72676f5e92d714
erol tuna <eroltuna86@gmail.com>
2021-07-26 12:14:18 +0300

    home profile done

--- js/main.js
+++ js/main.js
@@ -15,3 +15,25 @@ $('#mostsearch').owlCarousel({
         }
     }
 })
+
+
+
+$('#jobadv').owlCarousel({
+    loop:true,
+    margin:16,
+    nav:true,
+    dots:false,
+    items:6,
+    navText:['<i class="fa fa-arrow-left" aria-hidden="true"></i>','<i class="fa fa-arrow-right" aria-hidden="true"></i>'],
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:6
+        }
+    }
+})
